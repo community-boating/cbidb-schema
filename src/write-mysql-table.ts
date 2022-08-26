44ee8e21f1db64a3a25abc3e19e483fb82d24598
Jonathan Cole <jon@jcole.io>
2022-08-25 21:13:44 -0400

    \fixes for mysql

--- src/write-mysql-table.ts
+++ src/write-mysql-table.ts
@@ -16,14 +16,27 @@ function mapDataType(oracleType: string, length: number) {
 
 }
 
+const compositePrimaryKeys = {
+	"FLAGS_GLOBALS": "FLAG_ID, GLOBAL_ID",
+	"MEM_TYPES_AP_CLASS_TYPES": "MEMBERSHIP_TYPE_ID, AP_CLASS_TYPE_ID",
+	"SHOPPING_CART_APPL_GC": "CERT_ID, ORDER_ID",
+	"USERS_IPS": "USER_ID, IP_ADDRESS",
+	"FO_CLOSE_USERS": "CLOSE_ID, USER_ID, OPEN_CLOSE",
+	"SIGNOUT_SNAPSHOTS": "SNAPSHOT_DATETIME, PROGRAM_ID, BOAT_ID"
+}
+
 export default ({tableName, rows}: Table, pk: string, decimals: DecimalLookup) => {
 	let out = "";
 
 	out += "create table " + tableName + " (\n"
 	let delim = "";
+
+	const compositePk = compositePrimaryKeys[tableName]
+
 	rows.forEach(row => {
 		//TODO: mysql reserved word, drop this column
 		if (row.columnName == "LIMIT") return;
+
 
 		const isPk = row.columnName == pk
 		const isDecimal = decimals[tableName] && decimals[tableName][row.columnName];
@@ -34,17 +47,26 @@ export default ({tableName, rows}: Table, pk: string, decimals: DecimalLookup) =
 			"PERSONS_TEMP:ALLERGIES": "TEXT",
 			"PERSONS_TEMP:MEDICATIONS": "TEXT",
 			"PERSONS_TEMP:SPECIAL_NEEDS": "TEXT",
+			"DEVELOPER_SESSIONS:SESSION_ID":"BIGINT",
+			"PERSONS:DISCORD_USER_ID": "BIGINT",
+			"STRIPE_BALANCE_TRANSACTIONS:TRANSACTION_ID": "VARCHAR(50)"
 		}
+		const override = miscOverride[`${tableName}:${row.columnName}`]
 		const dataType = (function() {
-			const override = miscOverride[`${tableName}:${row.columnName}`]
-			if (isPk) return "INT";
+			
+			if (override != undefined) return override;
+		//	else if (isPk) return "INT";
 			else if (isDecimal) return "DECIMAL(8,2)";
-			else if (override != undefined) return override;
 			else return mapDataType(row.columnType, row.columnSize)
 		}()) 
-		out += `${delim} ${row.columnName} ${dataType} ${row.nullable ? "" : "NOT NULL"} ${isPk ? "AUTO_INCREMENT PRIMARY KEY" : ""}\n`
+		const autoIncrement = (isPk && !compositePk && override == undefined && dataType == "INT") ? "AUTO_INCREMENT" : "";
+		const pkStatement = (isPk && !compositePk && override == undefined ) ? "PRIMARY KEY" : "";
+		out += `${delim} ${row.columnName} ${dataType} ${row.nullable ? "" : "NOT NULL"} ${autoIncrement} ${pkStatement}\n`
 		delim = ","
 	})
+	if (compositePk) {
+		out += delim + "PRIMARY KEY (" + compositePk + ")\n"
+	}
 	out += ");\n\n"
 	return out;
 }
