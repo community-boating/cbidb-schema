fd45bf4b3d676c8328cebf9fc821e83529e56326
Jonathan Cole <jon@jcole.io>
2023-11-25 13:01:01 -0500

    add tags

--- src/serve.ts
+++ src/serve.ts
@@ -4,6 +4,15 @@ import * as swaggerUi from 'swagger-ui-express';
 import * as fs from 'fs'
 
 export default function serve() {
+	// https://github.com/swagger-api/swagger-ui/blob/master/docs/usage/configuration.md
+	const swaggerOptions: swaggerUi.SwaggerUiOptions = {
+		swaggerOptions: {
+			docExpansion: "none",
+			defaultModelExpandDepth: 6,
+			tagsSorter: "alpha"
+		}
+	}
+
 	const swaggerDocument = YAML.parse( fs.readFileSync('./out/api/combined.yaml', 'utf8'));
 	const app = express();
 	
@@ -17,7 +26,7 @@ export default function serve() {
 	})
 
 	
-	app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+	app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOptions));
 
 	console.log("Listening on port 8080")
 	app.listen(8080)
